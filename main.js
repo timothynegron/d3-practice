d2797b9432a629d264aead07f0da13e5df745e74
Timothy Negron <timothynegron.eet@gmail.com>
2021-04-16 10:46:15 -0400

    Adds another bar graph

--- main.js
+++ main.js
@@ -37,23 +37,23 @@ d3.select("#tex")
     .attr("x",0)
     .style("fill", "red");
 
-// ┌─────────────────────────────┐
-// │   Using variables with svg  │	
-// └─────────────────────────────┘
+// ┌────────────────────────────────┐
+// │   Using variables with d3 svg  │	
+// └────────────────────────────────┘
 
 const width = 200;
 const height = 100;
 const padding = 2;
-let dataset = [5, 10, 15, 20, 25];
-let svg = d3.select("#drawing")
+const dataset = [5, 10, 15, 20, 25];
+const svg = d3.select("#drawing")
                 .append("svg")
                 .attr("width", width)
                 .attr("height", height);
 
 // d for dataset
-svg.selectAll("#drawing")
+svg.selectAll("rect")
     .data(dataset)
-    .enter() // Create if don't exist
+    .enter() // Create if rect don't exist
     .append("rect")
     .attr("x", function(d, i){
         // i is indexes
@@ -67,4 +67,34 @@ svg.selectAll("#drawing")
     .attr("width", (width/dataset.length) - padding)
     .attr("height", function(d){
         return d * 4
+    });
+
+// ┌────────────────────────────────────────────┐
+// │   Using variables with d3 svg continuation │	
+// └────────────────────────────────────────────┘
+
+const width2 = 300; // Width of box
+const height2 = 350; // Height of box
+const padding2 = 2;
+const dataset2 = [10, 20, 30, 40, 50, 60, 70]
+const svg2 = d3.select("#drawing2").append("svg")
+                .attr("width", width2)
+                .attr("height", height2);
+
+svg2.selectAll("rect")
+    .data(dataset2)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i){ // Function will get called for each index
+        return i * (width2 / dataset2.length)
+    })
+    .attr("y", function(d){
+        return height2 - (d * 5);
+    })
+    .attr("width", width2 / dataset2.length - padding2)
+    .attr("height", function(d){
+        return d*5;
+    })
+    .attr("fill", function(d){
+        return "rgb(" + (d*3) + ", 0, 0)"
     });
